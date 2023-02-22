2fb9f86eddf3f1be4bc3468d183035ab975894e8
Bruno <brunomartins1030@gmail.com>
2023-02-22 10:43:25 -0300

    first commit

---
+++ src/main/java/com/bruno/sbootpdv/SbootPdvApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.sbootpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootPdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/sbootpdv/SbootPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.sbootpdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
