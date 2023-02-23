1ad4395770c65231e451037fc75c6efb4b4cdef6
Bruno <brunomartins1030@gmail.com>
2023-02-23 15:03:17 -0300

    creating User entity

---
+++ src/main/java/com/bruno/sbootpdv/entity/User.java
@@ -0,0 +1,18 @@
+package com.bruno.sbootpdv.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private boolean isEnable;
+
+}
