75e12642b4c50ad31d82a100d0b9d4764787f048
Bruno <brunomartins1030@gmail.com>
2023-05-18 09:11:03 -0300

    using lombok Builder

--- src/main/java/com/bruno/sbootpdv/dto/ProductInfoDTO.java
+++ src/main/java/com/bruno/sbootpdv/dto/ProductInfoDTO.java
@@ -1,12 +1,14 @@
 package com.bruno.sbootpdv.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProductInfoDTO {
 
     private long id;
--- src/main/java/com/bruno/sbootpdv/service/SaleService.java
+++ src/main/java/com/bruno/sbootpdv/service/SaleService.java
@@ -16,9 +16,11 @@ import com.bruno.sbootpdv.repository.SaleRepository;
 import com.bruno.sbootpdv.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -44,22 +46,25 @@ public class SaleService {
     }
 
     private SaleInfoDTO getSaleInfo(Sale sale){
-        SaleInfoDTO saleInfoDTO = new SaleInfoDTO();
-        saleInfoDTO.setUser(sale.getUser().getName());
-        saleInfoDTO.setDate(sale.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-        saleInfoDTO.setProducts(getProductInfo(sale.getItems()));
-
-        return saleInfoDTO;
+        return SaleInfoDTO.builder()
+                .user(sale.getUser().getName())
+                .date(sale.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .products(getProductInfo(sale.getItems()))
+                .build();
     }
 
     private List<ProductInfoDTO> getProductInfo(List<ItemSale> items){
-        return items.stream().map(item -> {
-            ProductInfoDTO productInfoDTO = new ProductInfoDTO();
-            productInfoDTO.setId(item.getId());
-            productInfoDTO.setDescription(item.getProduct().getDescription());
-            productInfoDTO.setQuantity(item.getQuantity());
-            return productInfoDTO;
-        }).collect(Collectors.toList());
+
+        if(CollectionUtils.isEmpty(items))
+            return Collections.emptyList();
+
+        return items.stream().map(item ->
+            ProductInfoDTO.builder()
+                    .id(item.getId())
+                    .description(item.getProduct().getDescription())
+                    .quantity(item.getQuantity())
+                    .build()
+        ).collect(Collectors.toList());
     }
 
     // essa anotacao @Transactional eh pra caso eu tenha algum problema no momento de salvar os itens da venda, e nao ficar com lixo na base, com uma venda que nao tem os itens salvos nela
